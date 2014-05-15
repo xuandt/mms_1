6a9f265022f74c81c97b3bfd89d821302c3f594f
dangthixuan <dangthixuan@dangthixuan-Inspiron-5520.(none)>
2014-05-15 11:45:45 +0700

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get "admin/user"
-  resources :skills
-  resources :projects
-  resources :positions
-  resources :teams
   resources :members
   resources :sessions, only: [:new, :create, :destroy]
 
@@ -11,15 +6,5 @@ Rails.application.routes.draw do
   match '/signup',  to: 'members#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-
-  namespace :admin do
-    resources :sessions, only: [:new, :create, :destroy]
-    resources :members
-    resources :projects
-    resources :positions
-    resources :teams
-    match "/signin", to: "sessions#new", via: "get"
-    match "/signout", to: "sessions#destroy", via: "delete"
-  end
 
 end
