aa48002e5969555e5f36ea0b84dbc846f988389d
dangthixuan <dangthixuan@dangthixuan-Inspiron-5520.(none)>
2014-05-20 15:45:12 +0700

    CRUD project

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@ end
 (1..5).each do |i|
 	Position.create(name: "Position#{i}", name: "PositionName#{i}", short_name: "PN#{i}")
 end
+
+(1..3).each do |i|
+  Team.create(name: "Team#{i}", description: "join to us", teamleader: "Teamleader#{i}")
+  (1..10).each do |j|
+    Project.create(name: "project#{j}", shortname: "Pr#{j}", start_date: "01-01-2000", end_date: "01-06-2000", project_leader: "prL#{j}", team_id: i)
+   end
+end
